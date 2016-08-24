7cf5e127e5c0034ce06f843c5bb527615a54e1b3
Mei-Hui Su <mei@Mei-Huis-MacBook-Air.local>
2016-08-23 17:05:32 -0700

    batch process the PLOTS json files

--- data/processIt.R
+++ data/processIt.R
@@ -1,10 +1,20 @@
 
 source("configIt.R")
-source("processCEL.R")
-
 configs <- setConfig_f()
-configs$sel <-c("E10.5_Mnd_D","E10.5_Mnd_P")
-#configs$comp <-"age"
-processCELdata_f(configs)
+
+slist<-c("E10.5_Mnd_D","E10.5_Mnd_P","E10.5_Max_D","E10.5_Max_P","E11.5_Mnd_D","E11.5_Mnd_P","E11.5_Max_D","E11.5_Max_P","E12.5_Mnd_D","E12.5_Mnd_P","E12.5_Max_D","E12.5_Max_P","E13.5_Mnd_D","E13.5_Mnd_P","E13.5_Max_D","E13.5_Max_P","E14.5_Mnd_D","E14.5_Mnd_P","E14.5_Max_D","E14.5_Max_P")
+
+c <- 1
+cc <- 2
+while ( cc < 20 ) { 
+  sel <-c(slist[c:cc])
+  c <- c+2
+  cc <- cc+2
+
+  configs$sel <-sel
+  print(configs$sel)
+  source("processCEL.R")
+  processCELdata_f(configs)
+}
 
 
